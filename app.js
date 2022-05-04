0127e898a2698ff5baae2853318c3fc4fad27d0b
Vattikuti Manideep Sitaram <manideepsitaram143@gmail.com>
2022-05-04 23:00:17 +0530

    Level 4

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //jshint esversion:6
 
-require('dotenv').config();
 const express=require('express');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
-const md5=require('md5');
+const bcrypt=require('bcrypt');
 
 const app=express();
 
@@ -23,6 +22,8 @@ const userSchema=new mongoose.Schema({
 
 const User=mongoose.model("User",userSchema);
 
+const saltRounds=10;
+
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -34,21 +35,19 @@ app.get('/login',(req,res)=>{
 
 app.post('/login',(req,res)=>{
     const reqUserName=req.body.username;
-    const password=md5(req.body.password);
+    const password=req.body.password;
 
-    User.findOne({userName:reqUserName},(err,user)=>{
+    User.findOne({userName:reqUserName},(err,foundUser)=>{
         if(err){
             console.log(err);
         }
         else{
-            if(user){
-                console.log(user);
-                if(user.password===password){
-                    res.render('secrets');
-                }
-                else{
-                    res.send("Not Found the USER Name");
-                }
+            if(foundUser){
+                bcrypt.compare(password,foundUser.password,(err,result)=>{
+                    if(result===true){
+                        res.render('secrets');
+                    }
+                })
             }
         }
     })
@@ -60,19 +59,23 @@ app.get('/register',(req,res)=>{
 
 app.post('/register',(req,res)=>{
 
-    const newUser=new User({
-        userName:req.body.username,
-        password:md5(req.body.password)
-    });
-    console.log(newUser);
-    newUser.save((err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render('secrets');
-        }
-    });
+    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+        const newUser=new User({
+            userName:req.body.username,
+            password:hash
+        });
+        newUser.save((err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render('secrets');
+            }
+        });
+
+    })
+
+
 });
 
 app.listen(3000,()=>{
